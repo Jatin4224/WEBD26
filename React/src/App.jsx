4d3b48182001f303ec0d4ac3f2fb30d2c157ad21
Jatin4224 <jaikumarsharma94130@gmail.com>
2026-04-21 21:43:05 +0530

    react basics

---
+++ React/src/App.jsx
@@ -0,0 +1,13 @@
+import One from "./lessons/module_01/One";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <One />
+      </div>
+    </>
+  );
+};
+
+export default App;
---
+++ React/src/lessons/module_01/One.jsx
@@ -0,0 +1,7 @@
+const One = () => {
+  return (
+    <div className="bg-white text-3xl flex justify-center mt-20">chapter</div>
+  );
+};
+
+export default One;
